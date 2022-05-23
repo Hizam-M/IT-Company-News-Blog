711c864427a9c274deb9f1ddf14123153f300ce4
Hizam Muharram <97117921+Hizam-M@users.noreply.github.com>
2022-05-23 10:22:33 -0400

    Update IT-Blog.js

--- IT-Blog.js
+++ IT-Blog.js
@@ -1,7 +1,4 @@
-  // search-box open close js code
-let navbar = document.querySelector(".navbar");
-let searchBox = document.querySelector(".search-box .bx-search");
-// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
 
 searchBox.addEventListener("click" ()=>{
   navbar.classList.toggle("showInput");
